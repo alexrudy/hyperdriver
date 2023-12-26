0514c69eec61ee20d548c06e953be5af055c0102
Alex Rudy <github@alexrudy.net>
2023-12-26 06:27:00 +0000

    Upgrade http_body to 1.0
    Upgrades http-body and http to version 1, introducing the http-body-util
    crate to provide combinators.

--- arnold/src/lib.rs
+++ arnold/src/lib.rs
@@ -4,13 +4,10 @@ use std::fmt;
 use std::pin::pin;
 use std::pin::Pin;
 
-#[cfg(feature = "hyper")]
-use futures::future::FutureExt;
-use http_body::combinators::UnsyncBoxBody;
-#[cfg(feature = "hyper")]
-use http_body::Body as _;
-
 use bytes::Bytes;
+use http_body_util::combinators::UnsyncBoxBody;
+use http_body_util::BodyExt;
+use http_body_util::{Empty, Full};
 
 type BoxError = Box<dyn std::error::Error + Sync + std::marker::Send + 'static>;
 
@@ -40,40 +37,6 @@ impl Default for Body {
     }
 }
 
-#[cfg(feature = "hyper")]
-impl From<hyper::Body> for Body {
-    fn from(body: hyper::Body) -> Self {
-        Self {
-            inner: InnerBody::Hyper(body),
-        }
-    }
-}
-
-#[cfg(feature = "hyper")]
-impl TryFrom<Body> for hyper::Body {
-    type Error = Body;
-    fn try_from(value: Body) -> Result<Self, Self::Error> {
-        match value.inner {
-            InnerBody::Empty => Ok(hyper::Body::empty()),
-            InnerBody::Full(mut full) => {
-                if let Some(Some(Ok(data))) = full.data().now_or_never() {
-                    Ok(hyper::Body::from(data))
-                } else {
-                    // One of three things happened here:
-                    // 1. The Future returned pending. But we know that can't happen for http_body::Full
-                    // 2. The Future returned None - meaning that someone else already polled this future.
-                    //    That can only happen while polling this future, which requires Full to be pinned,
-                    //    And we know that full is not pinned in this function (except where we poll it).
-                    // 3. The Future returned an error. But we know that can't happen for http_body::Full.
-                    panic!("Unexpected error while converting Body(Full) to hyper::Body")
-                }
-            }
-            InnerBody::Hyper(body) => Ok(body),
-            InnerBody::Boxed(_) => Err(value),
-        }
-    }
-}
-
 impl From<Bytes> for Body {
     fn from(body: Bytes) -> Self {
         Self {
@@ -96,16 +59,16 @@ impl From<&'static str> for Body {
     }
 }
 
-impl From<http_body::Full<Bytes>> for Body {
-    fn from(body: http_body::Full<Bytes>) -> Self {
+impl From<Full<Bytes>> for Body {
+    fn from(body: Full<Bytes>) -> Self {
         Self {
             inner: InnerBody::Full(body),
         }
     }
 }
 
-impl From<http_body::Empty<Bytes>> for Body {
-    fn from(_body: http_body::Empty<Bytes>) -> Self {
+impl From<Empty<Bytes>> for Body {
+    fn from(_body: Empty<Bytes>) -> Self {
         Self {
             inner: InnerBody::Empty,
         }
@@ -118,7 +81,7 @@ where
 {
     fn from(body: UnsyncBoxBody<Bytes, E>) -> Self {
         Self {
-            inner: InnerBody::Boxed(Box::pin(http_body::Body::map_err(body, Into::into))),
+            inner: InnerBody::Boxed(Box::pin(body.map_err(Into::into))),
         }
     }
 }
@@ -126,9 +89,7 @@ where
 #[pin_project::pin_project(project = InnerBodyProj)]
 enum InnerBody {
     Empty,
-    Full(#[pin] http_body::Full<Bytes>),
-    #[cfg(feature = "hyper")]
-    Hyper(#[pin] hyper::Body),
+    Full(#[pin] Full<Bytes>),
     Boxed(#[pin] Pin<Box<dyn http_body::Body<Data = Bytes, Error = BoxError> + Send + 'static>>),
 }
 
@@ -146,41 +107,25 @@ impl http_body::Body for Body {
     type Data = Bytes;
     type Error = BoxError;
 
-    fn poll_data(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Option<Result<Self::Data, Self::Error>>> {
-        match self.project().inner.project() {
-            InnerBodyProj::Empty => std::task::Poll::Ready(None),
-            #[cfg(feature = "hyper")]
-            InnerBodyProj::Hyper(body) => body
-                .poll_data(cx)
-                .map(|opt| opt.map(|res| res.map_err(Into::into))),
-            InnerBodyProj::Full(body) => body
-                .poll_data(cx)
-                .map(|opt| opt.map(|res| res.map_err(Into::into))),
-            InnerBodyProj::Boxed(body) => body.poll_data(cx),
-        }
-    }
-
-    fn poll_trailers(
+    fn poll_frame(
         self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<Option<http::HeaderMap>, Self::Error>> {
-        match self.project().inner.project() {
-            InnerBodyProj::Empty => std::task::Poll::Ready(Ok(None)),
-            #[cfg(feature = "hyper")]
-            InnerBodyProj::Hyper(body) => body.poll_trailers(cx).map(|res| res.map_err(Into::into)),
-            InnerBodyProj::Full(body) => body.poll_trailers(cx).map(|res| res.map_err(Into::into)),
-            InnerBodyProj::Boxed(body) => body.poll_trailers(cx),
+    ) -> std::task::Poll<Option<Result<http_body::Frame<Self::Data>, Self::Error>>> {
+        let this = self.project();
+        match this.inner.project() {
+            InnerBodyProj::Empty => std::task::Poll::Ready(None),
+            InnerBodyProj::Full(body) => body
+                .poll_frame(cx)
+                .map(|opt| opt.map(|res| res.map(|bytes| bytes.into()).map_err(Into::into))),
+            InnerBodyProj::Boxed(body) => body
+                .poll_frame(cx)
+                .map(|opt| opt.map(|res| res.map(|bytes| bytes.into()).map_err(Into::into))),
         }
     }
 
     fn is_end_stream(&self) -> bool {
         match self.inner {
             InnerBody::Empty => true,
-            #[cfg(feature = "hyper")]
-            InnerBody::Hyper(ref body) => body.is_end_stream(),
             InnerBody::Full(ref body) => body.is_end_stream(),
             InnerBody::Boxed(ref body) => body.is_end_stream(),
         }
@@ -189,8 +134,6 @@ impl http_body::Body for Body {
     fn size_hint(&self) -> http_body::SizeHint {
         match self.inner {
             InnerBody::Empty => http_body::SizeHint::with_exact(0),
-            #[cfg(feature = "hyper")]
-            InnerBody::Hyper(ref body) => body.size_hint(),
             InnerBody::Full(ref body) => body.size_hint(),
             InnerBody::Boxed(ref body) => body.size_hint(),
         }
@@ -202,8 +145,6 @@ impl fmt::Debug for InnerBody {
         match self {
             InnerBody::Empty => f.debug_struct("Empty").finish(),
             InnerBody::Full(_) => f.debug_struct("Full").finish(),
-            #[cfg(feature = "hyper")]
-            InnerBody::Hyper(_) => f.debug_struct("Hyper").finish(),
             InnerBody::Boxed(_) => f.debug_struct("Boxed").finish(),
         }
     }
