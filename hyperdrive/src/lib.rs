bbb2cabf8fc507585774b4d3ea60838b8bcf0e6d
Alex Rudy <github@alexrudy.net>
2024-03-25 01:17:25 +0000

    Add a metacrate to bring parts together.

---
+++ hyperdrive/src/lib.rs
@@ -0,0 +1,14 @@
+#[doc(inline)]
+pub use arnold as body;
+
+#[doc(inline)]
+pub use braid as stream;
+
+#[doc(inline)]
+pub use patron as client;
+
+#[doc(inline)]
+pub use platter as server;
+
+#[doc(inline)]
+pub use roomservice as discovery;
